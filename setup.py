a33ecd1030dbea7687a0a34f973e9c9b770672d4
Orien Zeng <ozeng@stanford.edu>
2016-11-12 08:28:17 -0800

    Change url

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='python-hello-world-flask',
+    name='foodpool',
     version='1.0.0',
-    description='Hello World app for running Python apps on Bluemix',
+    description='food delivery app',
     long_description=long_description,
-    url='https://github.com/IBM-Bluemix/python-hello-world-flask',
+    url='https://github.com/ofzeng/FoodPool-backend',
     license='Apache-2.0'
 )
